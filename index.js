318871316e5990b6a82acddecad76927116cccb1
Neha <nehaarun2005@gmail.com>
2026-06-15 12:18:43 +0530

    button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let box=document.querySelector("button")
 let mov=false  
+let predict=document.querySelector(".predict")
 box.addEventListener("mouseover",function(){  
     if(!mov){
         box.style.marginLeft ="100px"
@@ -10,3 +11,7 @@ box.addEventListener("mouseover",function(){
         mov=false
     }
 })
+setTimeout(function(){
+    box.style.display='none'
+    predict.innerText="sorryy....."
+},5000)
